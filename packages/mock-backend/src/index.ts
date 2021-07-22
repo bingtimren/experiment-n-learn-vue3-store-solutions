0b868b3982c2548c2a1f077f9040f8490cc8ace7
Bing Ren <bingtimren@gmail.com>
2021-07-22 10:48:24 +1000

    feat: make mocked todo item storage uses browser local storage and outputs persistence to console

--- packages/mock-backend/src/index.ts
+++ packages/mock-backend/src/index.ts
@@ -4,10 +4,32 @@ export interface TodoItem {
   completed: boolean;
 }
 
+const key = "MOCK_TODO_STORE";
+
 let storage: TodoItem[] = [];
+
+export function todosPrettyPrint() {
+  return storage
+    .map(
+      (todo, index) =>
+        `${index}: ${todo.title}${todo.completed ? " [DONE]" : ""}`
+    )
+    .join("\n");
+}
+
+export function retrieveItems() {
+  const stored = window.localStorage.getItem(key);
+  storage = stored ? JSON.parse(stored) : [];
+}
+
+function persist() {
+  window.localStorage.setItem(key, JSON.stringify(storage));
+  console.log(`Todo List Persisted:\n${todosPrettyPrint()}`);
+}
 
 export function createTodoItem(item: TodoItem) {
   storage.push(item);
+  persist();
 }
 
 export function getTodoItems(): TodoItem[] {
@@ -16,12 +38,14 @@ export function getTodoItems(): TodoItem[] {
 
 export function updateItems(items: TodoItem[]) {
   storage = items;
+  persist();
 }
 
 export function updateItem(item: TodoItem) {
   const itemInStore = storage.filter((i) => i.id === item.id);
   if (itemInStore.length === 1) {
     Object.assign(itemInStore[0], item);
+    persist();
   } else {
     throw Error("Not found");
   }
@@ -31,6 +55,7 @@ export function removeItemById(id: number) {
   const index = storage.findIndex((i) => i.id === id);
   if (index >= 0) {
     storage.splice(index, 1);
+    persist();
   } else {
     throw Error("Not found");
   }
